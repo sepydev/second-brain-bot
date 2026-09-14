0fc945c6fe8708a5ae0ff45622017369d1e9dd9b
Mohammad Rezazade <mrprocs@gmail.com>
2026-09-14 16:10:37 +0200

    feat: create settings class to read from .env file

---
+++ src/second_brain_bot/config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    telegram_bot_token:str
+
+    ollama_host:str = "http://localhost:11434"
+    ollama_model:str
+
+    second_brain_path:Path
+    database_path:Path = Path("data/reasearch.db")
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+
+settings = Settings()
